3e9cc179f02d7d9d421c435cf3411efb412cbd77
GT-Protocol <95014444+global-traders-protocol@users.noreply.github.com>
2022-01-13 12:17:59 +0500

    GTPVesting

--- contracts/GTPVesting.sol
+++ contracts/GTPVesting.sol
@@ -1,206 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.11;
 
-interface IBEP20 {
-  /**
-   * @dev Returns the amount of tokens in existence.
-   */
-  function totalSupply() external view returns (uint256);
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-  /**
-   * @dev Returns the token decimals.
-   */
-  function decimals() external view returns (uint8);
-
-  /**
-   * @dev Returns the token symbol.
-   */
-  function symbol() external view returns (string memory);
-
-  /**
-  * @dev Returns the token name.
-  */
-  function name() external view returns (string memory);
-
-  /**
-   * @dev Returns the bep token owner.
-   */
-  function getOwner() external view returns (address);
-
-  /**
-   * @dev Returns the amount of tokens owned by `account`.
-   */
-  function balanceOf(address account) external view returns (uint256);
-
-  /**
-   * @dev Moves `amount` tokens from the caller's account to `recipient`.
-   *
-   * Returns a boolean value indicating whether the operation succeeded.
-   *
-   * Emits a {Transfer} event.
-   */
-  function transfer(address recipient, uint256 amount) external returns (bool);
-
-  /**
-   * @dev Returns the remaining number of tokens that `spender` will be
-   * allowed to spend on behalf of `owner` through {transferFrom}. This is
-   * zero by default.
-   *
-   * This value changes when {approve} or {transferFrom} are called.
-   */
-  function allowance(address _owner, address spender) external view returns (uint256);
-
-  /**
-   * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-   *
-   * Returns a boolean value indicating whether the operation succeeded.
-   *
-   * IMPORTANT: Beware that changing an allowance with this method brings the risk
-   * that someone may use both the old and the new allowance by unfortunate
-   * transaction ordering. One possible solution to mitigate this race
-   * condition is to first reduce the spender's allowance to 0 and set the
-   * desired value afterwards:
-   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-   *
-   * Emits an {Approval} event.
-   */
-  function approve(address spender, uint256 amount) external returns (bool);
-
-  /**
-   * @dev Moves `amount` tokens from `sender` to `recipient` using the
-   * allowance mechanism. `amount` is then deducted from the caller's
-   * allowance.
-   *
-   * Returns a boolean value indicating whether the operation succeeded.
-   *
-   * Emits a {Transfer} event.
-   */
-  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-  /**
-   * @dev Emitted when `value` tokens are moved from one account (`from`) to
-   * another (`to`).
-   *
-   * Note that `value` may be zero.
-   */
-  event Transfer(address indexed from, address indexed to, uint256 value);
-
-  /**
-   * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-   * a call to {approve}. `value` is the new allowance.
-   */
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-/*
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with GSN meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-abstract contract Context {
-  // Empty internal constructor, to prevent people from mistakenly deploying
-  // an instance of this contract, which should be used via inheritance.
-  constructor () internal { }
-
-  function _msgSender() internal view returns (address) {
-    return msg.sender;
-  }
-
-  function _msgData() internal view returns (bytes memory) {
-    this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-    return msg.data;
-  }
-}
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-contract Ownable is Context {
-  address private _owner;
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-  /**
-   * @dev Initializes the contract setting the deployer as the initial owner.
-   */
-  constructor () {
-    address msgSender = _msgSender();
-    _owner = msgSender;
-    emit OwnershipTransferred(address(0), msgSender);
-  }
-
-  /**
-   * @dev Returns the address of the current owner.
-   */
-  function owner() public view returns (address) {
-    return _owner;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(_owner == _msgSender(), "Ownable: caller is not the owner");
-    _;
-  }
-
-  /**
-   * @dev Leaves the contract without owner. It will not be possible to call
-   * `onlyOwner` functions anymore. Can only be called by the current owner.
-   *
-   * NOTE: Renouncing ownership will leave the contract without an owner,
-   * thereby removing any functionality that is only available to the owner.
-   */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipTransferred(_owner, address(0));
-    _owner = address(0);
-  }
-
-  /**
-   * @dev Transfers ownership of the contract to a new account (`newOwner`).
-   * Can only be called by the current owner.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    _transferOwnership(newOwner);
-  }
-
-  /**
-   * @dev Transfers ownership of the contract to a new account (`newOwner`).
-   */
-  function _transferOwnership(address newOwner) internal {
-    require(newOwner != address(0), "Ownable: new owner is the zero address");
-    emit OwnershipTransferred(_owner, newOwner);
-    _owner = newOwner;
-  }
-}
-
-contract GTPVesting is Ownable {
+contract Vesting is Ownable {
+    using SafeERC20 for IERC20;
 
     /// @notice GTP token contract
-    IBEP20 public immutable gtp;
-
-    /// @notice Timestamp of the vesting begin time
-    uint256 public immutable vestingBegin;
-
-    /// @notice Timestmap of the vesting end time
-    uint256 public immutable vestingEnd;
+    IERC20 public immutable gtp;
 
     struct VestingParams {
         uint256 amount;
+        uint256 vestingBegin;
+        uint256 vestingEnd;
         uint256 lastUpdate;
         uint256 claimed;
     }
@@ -221,22 +35,18 @@ contract GTPVesting is Ownable {
         address recipient;
         uint256 amount;
         uint256 unlocked;
+        uint256 vestingBegin;
+        uint256 vestingEnd;
     }
 
     // CONSTRUCTOR
 
     /**
      * @notice Contract constructor
-     * @param gtp_ Address of the GTP contract
-     * @param vestingBegin_ Timestamp of the vesting begin time
-     * @param vestingEnd_ Timestamp of the vesting end time
+     * @param gtp_ Address of the GTP token contract
      */
-    constructor(IBEP20 gtp_, uint256 vestingBegin_, uint256 vestingEnd_) Ownable() {
-        require(vestingEnd_ > vestingBegin_, "Vesting: vesting end should be greater than vesting begin");
-
+    constructor(IERC20 gtp_) Ownable() {
         gtp = gtp_;
-        vestingBegin = vestingBegin_;
-        vestingEnd = vestingEnd_;
     }
 
     /**
@@ -253,9 +63,9 @@ contract GTPVesting is Ownable {
                 vestings[vestingIds[account][i]].lastUpdate = block.timestamp;
             }
         }
-        require(gtp.transfer(account, totalAmount), "Vesting::claim: transfer error");
+        gtp.safeTransfer(account, totalAmount);
     }
-    
+
     // RESTRICTED FUNCTIONS
 
     /**
@@ -267,7 +77,7 @@ contract GTPVesting is Ownable {
         for (uint8 i = 0; i < params.length; i++) {
             totalAmount += params[i].amount;
         }
-        require(gtp.transferFrom(msg.sender, address(this), totalAmount), "Vesting::holdTokens: transfer failed");
+        gtp.safeTransferFrom(msg.sender, address(this), totalAmount);
         totalVest += totalAmount;
         for (uint8 i = 0; i < params.length; i++) {
             _holdTokens(params[i]);
@@ -279,7 +89,11 @@ contract GTPVesting is Ownable {
      * @param account Account to calculate amount for
      * @return amount Total amount of available tokens
      */
-    function getAvailableBalanceOf(address account) external view returns (uint256 amount) {
+    function getAvailableBalanceOf(address account)
+        external
+        view
+        returns (uint256 amount)
+    {
         for (uint8 i = 0; i < vestingIds[account].length; i++) {
             amount += getAvailableBalance(vestingIds[account][i]);
         }
@@ -293,15 +107,18 @@ contract GTPVesting is Ownable {
     function getAvailableBalance(uint256 id) public view returns (uint256) {
         VestingParams memory vestParams = vestings[id];
         uint256 amount;
-        if (block.timestamp >= vestingEnd) {
+        if (block.timestamp >= vestParams.vestingEnd) {
             amount = vestParams.amount - vestParams.claimed;
         } else {
-            amount = vestParams.amount * (block.timestamp - vestParams.lastUpdate) / (vestingEnd - vestingBegin);
+            amount =
+                (vestParams.amount *
+                    (block.timestamp - vestParams.lastUpdate)) /
+                (vestParams.vestingEnd - vestParams.vestingBegin);
         }
         return amount;
     }
 
-     /**
+    /**
      * @notice Function gets amount of vesting objects for account
      * @param account Address of account
      * @return Amount of vesting objects
@@ -317,16 +134,28 @@ contract GTPVesting is Ownable {
      * @param params HoldParams object with vesting params
      */
     function _holdTokens(HoldParams memory params) private {
-        require(params.amount > 0, "Vesting::holdTokens: can not hold zero amount");
-        require(params.unlocked <= params.amount, "Vesting::holdTokens: unlocked can not be greater than amount");
-    
+        require(
+            params.amount > 0,
+            "Vesting::holdTokens: can not hold zero amount"
+        );
+        require(
+            params.vestingEnd > params.vestingBegin,
+            "Vesting::holdTokens: end earlier than begin"
+        );
+        require(
+            params.unlocked <= params.amount,
+            "Vesting::holdTokens: unlocked can not be greater than amount"
+        );
+
         if (params.unlocked > 0) {
             gtp.transfer(params.recipient, params.unlocked);
         }
         if (params.unlocked < params.amount) {
             vestings[_nextVestingId] = VestingParams({
                 amount: params.amount - params.unlocked,
-                lastUpdate: vestingBegin,
+                vestingBegin: params.vestingBegin,
+                vestingEnd: params.vestingEnd,
+                lastUpdate: params.vestingBegin,
                 claimed: 0
             });
             vestingIds[params.recipient].push(_nextVestingId);
